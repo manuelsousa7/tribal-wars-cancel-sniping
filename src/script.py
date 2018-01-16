e002fdf9b1faf04489542fe2a4808dd7387a9d67
Manuel Sousa <manuelsousamvs@gmail.com>
2018-01-16 15:14:15 +0000

    final script

--- src/script.py
+++ src/script.py
@@ -1,6 +1,6 @@
 import sys
 import datetime
-
+import re
 
 
 def get_hours(arg):
@@ -15,21 +15,22 @@ def get_seconds(arg):
 	(h, m, s) = arg.split(':')
 	return int(s)
 
-def check_arguments(arg):
-	return True
 
-if(len(sys.argv) != 4 and check_arguments(sys.argv)):
-	print("error")
-	sys.exit()
+def main():
+	if(len(sys.argv) != 4):
+		print("error")
+		sys.exit()
+
+	arrivalATSUPtime = datetime.timedelta(hours=get_hours(sys.argv[1]), minutes=get_minutes(sys.argv[1]), seconds=get_seconds(sys.argv[1]))
+	ATSUPtimeduration = datetime.timedelta(hours=get_hours(sys.argv[2]), minutes=get_minutes(sys.argv[2]), seconds=get_seconds(sys.argv[2]))
+	NTarrivaltime = datetime.timedelta(hours=get_hours(sys.argv[3]), minutes=get_minutes(sys.argv[3]), seconds=get_seconds(sys.argv[3]))
+
+	count = datetime.timedelta()
+
+	count = (NTarrivaltime - (arrivalATSUPtime - ATSUPtimeduration)) / 2 + (arrivalATSUPtime-ATSUPtimeduration) # Algorithm
+
+	return str(count)
 
 
-arrivalATSUPtime = datetime.timedelta(hours=get_hours(sys.argv[1]), minutes=get_minutes(sys.argv[1]), seconds=get_seconds(sys.argv[1]))
-ATSUPtimeduration = datetime.timedelta(hours=get_hours(sys.argv[2]), minutes=get_minutes(sys.argv[2]), seconds=get_seconds(sys.argv[2]))
-NTarrivaltime = datetime.timedelta(hours=get_hours(sys.argv[3]), minutes=get_minutes(sys.argv[3]), seconds=get_seconds(sys.argv[3]))
-
-
-count = datetime.timedelta()
-
-count = (NTarrivaltime - (arrivalATSUPtime - ATSUPtimeduration)) / 2 + (arrivalATSUPtime-ATSUPtimeduration) # Algorithm
-
-print(str(count))
+if __name__ == "__main__":
+    print main()
